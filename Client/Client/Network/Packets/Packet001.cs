d2c086f8e73c2e70f2e5b0a2b246a87b7abe2d76
James Harrinton <jrileyharrington@gmail.com>
2016-01-24 19:46:37 -0600

    C# packet client and server
    socket code

---
+++ Client/Client/Network/Packets/Packet001.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client.Network.Packets
+{
+    [Serializable]
+    public class Packet001 : Packet
+    {
+        private string message;
+        private List<string> messageLog;
+
+        public Packet001(string msg)
+        {
+            packetName = "MessagePacket";
+            message = msg;
+        }
+        public Packet001(string msg, ConsoleColor color)
+        {
+            foreground = color;
+            packetName = "MessagePacket";
+            message = msg;
+        }
+        public Packet001(string msg, List<string> log)
+        {
+            packetName = "MessagePacket";
+            message = msg;
+            messageLog = log;
+        }
+
+        public string getMessage()
+        {
+            return message;
+        }
+
+        public List<string> getLog()
+        {
+            return messageLog;
+        }
+
+        public string getLogMessage(int index)
+        {
+            return messageLog.ElementAt(index);
+        }
+    }
+}
---
+++ Client/Client/Network/Packets/packet999.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Client.Network.Packets
+{
+    [Serializable]
+    public class Packet999 : Packet
+    {
+        public Packet999()
+        {
+            packetName = "DisconnectPacket";
+        }
+    }
+}
